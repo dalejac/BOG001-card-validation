87b2e7fac39e5837c5f9da116094f974efa1622d
Alejandra Cruz <k-da929@hotmail.com>
2020-06-03 10:13:24 -0500

    Versión 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// Dar función al botón para ocultar caja
+function continuarPago(){
+    document.getElementById('caja').style.display='none'
+}
+
 import validator from './validator.js';
 
 console.log(validator);
+
