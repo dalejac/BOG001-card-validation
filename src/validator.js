a964e1ab619fba9725a7dd5801aea7f2fed59e5e
Caro <cslondo90@gmail.com>
2020-06-07 09:14:53 -0500

    arreglo del html semántico

--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,14 @@
 const validator = {
 
   isValid: function(creditCardNumber) {
+
+    respuestaPrueba1=creditCardNumber+1
       
   },
 
   maskify: function(creditCardNumber) {
+
+    respuestaPrueba2=creditCardNumber+2
       
 
   }
